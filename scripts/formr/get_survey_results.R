434355a95c9184f0e9596b94b78d0e6e71cf0189
Nick Christie <30588830+n-christie@users.noreply.github.com>
2024-02-12 16:49:19 +0100

    analyzed ahrg test survey results.

--- scripts/formr/get_survey_results.R
+++ scripts/formr/get_survey_results.R
@@ -3,6 +3,10 @@ library(tidyverse)
 library("logitr")
 library(cbcTools)
 library(tidyr)
+library(texreg)
+library(likert)
+library(stringr)
+library(here)
 source(".passwords.R")
 
 formr::formr_connect(credentials$email,
@@ -10,7 +14,7 @@ formr::formr_connect(credentials$email,
                      host = "http://37.27.25.127")
 
 
-# get survey data
+# get survey data ----
 
 survey_design <- read_csv("https://github.com/n-christie/ahrg_dce_survey/blob/main/output/formr/choice_questions.csv?raw=true")
 
@@ -26,21 +30,73 @@ s_data_p4 <- formr::formr_results("reloc_dce_p4",
 s_data_feedback <- formr::formr_results("reloc_dce_feedbak",
                                      host = "http://37.27.25.127")
 
-saveRDS(s_data_feedback, file = "feedback_test.rds")
+survey_df <- s_data_p1 %>%
+  left_join(s_data_feedback,
+            join_by(session)) %>% 
+  filter(!is.na(ended.y)) %>%  # finished the survey
+  #filter(str_detect(yearOfBirth,"^[:digit:]+$")) # must have 4 digit birth year
+  left_join(s_data_p2,
+            join_by(session)) %>% 
+  left_join(s_data_p4,
+            join_by(session))
 
-# tidy feedback data (NOT WORKING)
 
-library(likert)
+saveRDS(survey_df, file = "ahrg_test.rds")
+saveRDS(survey_design, file = "survey_design.rds")
 
+# prepare tables/figures for shinydoc ----
+
+
+
+
+# tidy feedback data ----
+
+df_feed <- s_data_feedback %>% 
+  filter(!is.na(ended)) %>% 
+  select(feedback11) %>% 
+  sjlabelled::label_to_colnames() %>% 
+  mutate(across(.cols = everything(), ~factor(.x, levels = 1:2, labels = c("Mobile", "Desktop")))) %>% 
+  sjlabelled::label_to_colnames() %>% 
+  as.data.frame()
+
+
+plot(likert(df_feed),
+     legend.position = 'right') +  
+  theme(legend.text = element_text(size = rel(0.7)))
+
+png(file = here("output/dce_shiny_doc/figures","phone.png"),
+    width = 1300, height = 400,)
+p1
+dev.off()
+
+
+
+
+dff2 <- s_data_feedback %>% 
+  filter(!is.na(ended)) %>% 
+  select(contains ("rating_button")) %>% 
+  sjlabelled::label_to_colnames() %>% 
+  mutate(across(.cols = everything(), ~factor(.x, levels = 1:5))) %>% 
+  sjlabelled::label_to_colnames() %>% 
+  as.data.frame() 
+
+  plot(likert(dff2))
+
+  
+## Attribute feedback
+  
+  
+  
 df_plot <- s_data_feedback %>% 
   select(rating_button01,
          rating_button02) %>% 
   sjlabelled::label_to_colnames() %>% 
+  na.omit() %>% 
   mutate(across(where(is.numeric), ~as.factor(., levels = c("1", "2", "3", "4", "5")))) %>%
   as.data.frame() 
 
 df_plot <- s_data_feedback %>% 
-  select(rating_button01, rating_button02) %>% 
+  select(rating_button01, rating_button02) %>%
   sjlabelled::label_to_colnames() %>% 
   mutate(across(.cols = where(is.numeric), ~as.factor(.), .names = "factor_{.col}")) %>%
   as.data.frame()
@@ -61,7 +117,7 @@ data_rand <- cbc_choices(
 ) 
 
 # data tidying
-df_survey <- survey_data_w_full_info %>% 
+df_survey <- s_data_p4 %>% 
   dplyr::filter(!is.na(ended)) %>%
   select(respondentID,
          cbc1,
@@ -95,3 +151,4 @@ m1_dopt <- logitr(
 )
 
 summary(m1_dopt)
+screenreg(m1_dopt)
