3a9621a3945d621d255e3428ce382bd90abfb0f1
Nick Christie <30588830+n-christie@users.noreply.github.com>
2025-09-29 16:00:32 +0200

    added some method text and reorganized some other text

--- scripts/paper/baseline_regs.R
+++ scripts/paper/baseline_regs.R
@@ -80,7 +80,7 @@ cost_name   <- "price_num"
 set.seed(12345)  # or any fixed number
 
 mnl <- logitr(
-  data    = df_model %>% filter(Own != "Owner") ,
+  data    = df_model %>% filter(age_group == "75+") ,
   outcome = "choice",
   obsID   = "obsID",
   panelID = "panelID",
@@ -88,7 +88,7 @@ mnl <- logitr(
 )
 
 mxl <- logitr(
-  data    = df_model %>% filter(Own != "Owner"),
+  data    = df_model %>% filter(age_group == "75+"),
   outcome = "choice",
   obsID   = "obsID",
   panelID = "panelID",
@@ -375,13 +375,34 @@ texreg(
   dcolumn = TRUE,
   use.packages = FALSE,
   na.replace = "--",  
-  caption = "Mixed Logit Estimates for Renters: Base Specification",  
+  caption = "Mixed Logit Estimates for 75+ : Base Specification",  
   caption.above = TRUE,
   fontsize = "scriptsize",
-  file = here("docs/elsvier/tables","base_reg_renter.tex") 
+  file = here("docs/elsvier/tables","base_reg_75.tex") 
   
 )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Age groups ----
 
 
 
--- scripts/paper/descriptive_table.R
+++ scripts/paper/descriptive_table.R
@@ -17,10 +17,32 @@ dce_survey_df <- readRDS(here("data/formr", "08_02_results.rds")) %>%
 
 sample_df <- readRDS(here("data/formr", "dce_anym_output_1.rds"))
 
+# Add other survey data ----
+t3_sur <- readRDS("~/Projects/!med/ahrg_reloc_age_dce/data/rt3_eng.rds") %>% 
+  select(RespondentID,
+         VAR174_8,
+         VAR010,
+         VAR011,
+         VAR022,
+         VAR028_1,
+         VAR028_2,
+         VAR028_3,
+         VAR033_1,
+         VAR033_2,
+         VAR035,
+         VAR162,
+         VAR163
+         
+         
+         
+  )
+
 
 survey_df <- sample_df %>% 
   left_join(dce_survey_df,
             by = c("Användarnamn" = "user_code")) %>% 
+  left_join(t3_sur,
+            by = c("RespondentID")) %>% 
   distinct(Användarnamn, .keep_all = TRUE) %>% 
   mutate(respondentID = respondentID.y)
 
@@ -31,34 +53,52 @@ survey_df <- sample_df %>%
 dfSum <- survey_df %>% 
   filter(
     !is.na(Sex)
-    # planed_cost != 0,
-    #      monthcost != 0,
-    #      income != 0
          ) %>%
   select(monthcost,
          income,
          planed_cost,
-         Age_T3,
-         Sex
-         
+         Age = Age_T3,
+         Sex,
+         civil_status_T2,
+         ägandebostad,
+         bostadstyp,
+         VAR174_8,
+         VAR010,
+         VAR035
   )%>% 
-  mutate(Sex = factor(Sex)) %>% 
-  # mutate(time_page_1 = time_page_1/60,
-  #        cbc_time = (if_else(is.na(time_page_4),time_page_2,time_page_4)/60) ,
-  #        total_survey_time = rowSums(across(c(time_page_1,cbc_time))) ) %>% 
+  mutate(Sex = factor(if_else(Sex == 1, "Male", "Female" )),
+         age_group = factor(case_when(
+           Age >= 55 & Age < 65 ~ "55-64",
+           Age >= 65 & Age < 75 ~ "65-74",
+           Age >= 75 ~ "75+",
+         )),
+         civil_status_T2 = if_else(civil_status_T2 == 1, "Partnered","Not partnered"),
+         ägandebostad = haven::as_factor(ägandebostad),
+         bostadstyp = haven::as_factor(bostadstyp),
+         Own = factor(if_else(ägandebostad == "Ja", "Owner", "Renter")),
+         Hus = factor(if_else( bostadstyp == "Friliggande villa/hus/gård" | bostadstyp == "Radhus/kedjehus/parhus", "House", "Apartment/Condo")),
+         Retired = haven::as_factor(VAR174_8),
+         location = haven::as_factor(VAR010),
+         health = haven::as_factor(VAR035),
+) %>% 
   as.data.frame()
 
+label(dfSum$Hus) <- "Current housing type"
 
+label(dfSum$Own) <- "Current housing"
+label(dfSum$age_group) <- "Age group"
+label(dfSum$location) <- "Current housing location"
+label(dfSum$Retired) <- "Retired"
 label(dfSum$monthcost) <- "Monthly costs"
 label(dfSum$income) <- "Monthly household income"
-# label(dfSum$cbc_time) <- "Time to complete DCE section (min)"
+ label(dfSum$civil_status_T2) <- "Civil status"
 # label(dfSum$total_survey_time) <- "Time to complete entire experiment (min)"
 label(dfSum$planed_cost) <- "Planned housing costs"
 
 
 
 
-t1 <- table1::table1(~ monthcost + income + planed_cost + Age_T3 + Sex  ,
+t1 <- table1::table1(~ Sex + age_group + civil_status_T2 + Retired + Own + Hus + location + income + planed_cost    ,
                      data = dfSum,
                      na.rm = TRUE,
                      digits = 3,
@@ -66,6 +106,8 @@ t1 <- table1::table1(~ monthcost + income + planed_cost + Age_T3 + Sex  ,
                      # extra.col=list(`P-value`=pvalue),
                      caption = "Sample description") 
 
+
 table1::t1flex(t1) |> 
   flextable::fontsize(size = 11) |> 
   flextable::padding(padding = 1, part = "all") 
+
